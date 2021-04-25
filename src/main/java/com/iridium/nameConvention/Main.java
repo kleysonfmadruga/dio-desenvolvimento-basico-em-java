98d4c307490469378ed3bf7b78f4e178f316d082
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-04-25 17:11:03 -0300

    Adiciona exercício de convenção de nomes em Java

---
+++ src/main/java/com/iridium/nameConvention/Main.java
@@ -0,0 +1,18 @@
+package com.iridium.nameConvention;
+// Package names should be written in 'camel case', with the first letter lowercase
+
+// Class, Interface and Enum names should be written in 'camel case', with the first letter uppercase
+public class Main {
+    // Constant names should be written in 'snake case', with all letters uppercase
+    final int ANSWER_TO_LIFE_UNIVERSE_AND_EVERYTHING = 42;
+    // Attribute names should be written in 'camel case', with the first letter lowercase
+    boolean javaIsCool = true;
+
+    // Method names should be written in 'camel case', with the first letter lowercase
+    public static void main(String[] args){
+        // Variable names should be written in 'camel case', with the first letter lowercase
+        String myName = "Hello world, my name is Kleyson";
+
+        System.out.println(myName);
+    }
+}
